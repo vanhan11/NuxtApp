365bcad02ee26b1e70f341baec117459c04cb5b2
vanhan200902 <vanhan200902@gmail.com>
2021-12-05 23:24:43 +0700

    nuxt app cli vue

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <section class="container">
+    <div class="m-10" >
+      <center>
+        <app-logo/>
+      <h2 class="subtitle">
+        HELLO WORLD
+      </h2>
+      </center>
+    </div>
+  </section>
+</template>
+
+<script>
+import AppLogo from '~/components/AppLogo.vue'
+
+export default {
+  components: {
+    AppLogo
+  }
+}
+</script>
